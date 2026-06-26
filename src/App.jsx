523945a52a9d8a1da31a1105620cc84c4df0affd
lester-mtse-machine <lesterc013@gmail.com>
2026-06-26 15:17:26 +0800

    Add Form and Display components to entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import './App.css';
+
+import Display from './components/Display';
+import Form from './components/Form';
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className='app'>
+      <Form />
+      <Display />
+    </div>
   );
 }
 
