a821344dd636a02db675dfd286e714e1b92685ee
lester-mtse-machine <lesterc013@gmail.com>
2026-06-26 15:56:16 +0800

    Add General Section to Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,16 @@
+import Input from './form/Input';
 import Section from './form/Section';
 
 export default function Form() {
   return (
     <div className='form'>
       <h1 className='form-header'>Fill In Details</h1>
-      <Section title={'General'} />
+      {/* How to reuse Section? Use children prop instead of making GeneralSection ExperienceSection etc */}
+      <Section title={'General'}>
+        <Input label={'Name'} id={'name'} type={'text'} />
+        <Input label={'Email'} id={'email'} type={'email'} />
+        <Input label={'Tel'} id={'tel'} type={'tel'} />
+      </Section>
       <Section title={'Experience'} />
       <Section title={'Education'} />
     </div>
