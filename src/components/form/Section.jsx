89eeb35926d25e6c9a88cb3a95f394259f61ebb8
lester-mtse-machine <lesterc013@gmail.com>
2026-06-29 15:20:31 +0800

    del: Remove Section as not required

--- src/components/form/Section.jsx
+++
@@ -1,8 +0,0 @@
-export default function Section({ title, children }) {
-  return (
-    <div className='form-section'>
-      <h2 className='title'>{title}</h2>
-      {children}
-    </div>
-  );
-}
