94acd147c5779af7ebcf6154ea9c5ee123f14f0c
lester-mtse-machine <lesterc013@gmail.com>
2026-06-29 17:22:43 +0800

    feat: Render each Input within ExpFormBlock
    Using Input has more pros since its a simple abstraction vs the incovenience of one more layer of prop drilling.

--- src/components/form/ExpFormBlock.jsx
+++ src/components/form/ExpFormBlock.jsx
@@ -1,13 +1,54 @@
 import Input from './Input';
 
-export default function ExpFormBlock() {
+export default function ExpFormBlock({ experienceDataObj }) {
+  const blockId = experienceDataObj.id;
+
   return (
-    <div className='exp-form-block'>
-      <Input label={'Company'} id={'company'} type={'text'} />
-      <Input label={'Position'} id={'position'} type={'text'} />
-      <Input label={'Start'} id={'startDate'} type={'text'} />
-      <Input label={'End'} id={'endDate'} type={'text'} />
-      <Input label={'Description'} id={'description'} type={'textarea'} />
+    <div className='exp-form-block' id={experienceDataObj.id}>
+      <Input
+        inputRowId={`${blockId}-company`}
+        labelDisplay={'Company'}
+        inputId={`${blockId}-company`}
+        type={'text'}
+        value={experienceDataObj.company}
+        // handleValueChanged={}
+      />
+
+      <Input
+        inputRowId={`${blockId}-position`}
+        labelDisplay={'Position'}
+        inputId={`${blockId}-position`}
+        type={'text'}
+        value={experienceDataObj.position}
+        // handleValueChanged={}
+      />
+
+      <Input
+        inputRowId={`${blockId}-startDate`}
+        labelDisplay={'Start Date'}
+        inputId={`${blockId}-startDate`}
+        type={'text'}
+        value={experienceDataObj.startDate}
+        // handleValueChanged={}
+      />
+
+      <Input
+        inputRowId={`${blockId}-endDate`}
+        labelDisplay={'End Date'}
+        inputId={`${blockId}-endDate`}
+        type={'text'}
+        value={experienceDataObj.endDate}
+        // handleValueChanged={}
+      />
+
+      <Input
+        inputRowId={`${blockId}-description`}
+        labelDisplay={'Description'}
+        inputId={`${blockId}-description`}
+        type={'textarea'}
+        value={experienceDataObj.description}
+        // handleValueChanged={}
+      />
     </div>
   );
 }
--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -1,19 +1,35 @@
-export default function Input({ label, id, type }) {
+export default function Input({
+  inputRowId,
+  labelDisplay,
+  inputId,
+  type,
+  value,
+  handleValueChanged,
+}) {
   const textareaRows = 5;
   const textareaCols = 30;
 
   return (
-    <div className='input-row'>
-      <label htmlFor={id}>{label}: </label>
+    <div className='input-row' id={inputRowId}>
+      <label htmlFor={inputId}>{labelDisplay}: </label>
       {type === 'textarea' ? (
         <textarea
-          name={id}
-          id={id}
+          name={inputId}
+          id={inputId}
           rows={textareaRows}
           cols={textareaCols}
+          value={value}
+          onChange={(e) => handleValueChanged(e)}
         ></textarea>
       ) : (
-        <input type={type} id={id} name={id} autoComplete='on' />
+        <input
+          type={type}
+          id={inputId}
+          name={inputId}
+          autoComplete='on'
+          value={value}
+          onChange={(e) => handleValueChanged(e)}
+        />
       )}
     </div>
   );
