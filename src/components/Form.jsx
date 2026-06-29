67a099f367dd4ad961bb6ad1013ad320a3ef6f96
lester-mtse-machine <lesterc013@gmail.com>
2026-06-29 15:21:29 +0800

    Set static display of Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,27 @@
+import ExpFormBlock from './form/ExpFormBlock';
 import Input from './form/Input';
-import Section from './form/Section';
 
 export default function Form() {
   return (
     <div className='form'>
       <h1 className='form-header'>Fill In Details</h1>
-      {/* How to reuse Section? Use children prop instead of making GeneralSection ExperienceSection etc */}
-      <Section title={'General'}>
+
+      <div className='personal-details'>
+        <h2>Personal Details</h2>
         <Input label={'Name'} id={'name'} type={'text'} />
         <Input label={'Email'} id={'email'} type={'email'} />
         <Input label={'Tel'} id={'tel'} type={'tel'} />
-      </Section>
-      <Section title={'Experience'} />
-      <Section title={'Education'} />
+      </div>
+
+      <div className='experience'>
+        <h2>Experience</h2>
+        <ExpFormBlock />
+      </div>
+
+      <div className='education'>
+        <h2>Education</h2>
+        {/* Use reusable blocks here */}
+      </div>
     </div>
   );
 }
